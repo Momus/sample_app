74e2e880d45aa78b7e7ab4fa1af16fbe0334f334
Dmitri G. Brengauz <dmitri@momus.net>
2015-11-12 16:19:55 -0700

    Completed 3.3 "Our first test" GREEN

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,12 @@
 class StaticPagesController < ApplicationController
 
-  attr_accessor :secret_word
-  
   def home
-    @secret_word = "FOO!"
   end
 
   def help
   end
+
+  def about
+  end
+
 end
