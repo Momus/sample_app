10bd8bb7b9e2b91d3e590477bd360ec472129012
Dmitri G. Brengauz <dmitri@momus.net>
2015-12-14 14:08:56 -0700

    Finished with 6.2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,6 +50,14 @@ class UserTest < ActiveSupport::TestCase
     
   end
 
+  test "email address is unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not  duplicate_user.valid?
+  end
+
+
   
 end
   
