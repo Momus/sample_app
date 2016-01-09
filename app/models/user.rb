c09ff127cb3b10978e8ea688016cd23ba61fbf45
Dmitri G. Brengauz <dmitri@momus.net>
2016-01-08 21:32:18 -0700

    Testing layout changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,13 @@ class User < ActiveRecord::Base
             presence: true,
             length: {minimum: 6, maximum: 72}
 
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+    
+  end
+  
 end
