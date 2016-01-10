43090f70bdb5efaf4e185293e2a204fa289afa01
Dmitri G. Brengauz <dmitri@momus.net>
2016-01-09 19:21:35 -0700

    Remember token and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 
+  attr_accessor :remember_token
   
   validates :name,
             presence: true,
@@ -28,5 +29,13 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
     
   end
-  
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(@remember_token))
+  end
 end
