66a234529f2a9c7f7600cb7fc4b51e4685947a9d
Dmitri G. Brengauz <dmitri@momus.net>
2016-01-15 11:14:43 -0700

    Remember tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,7 +38,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   end
 
-  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
 
   
 end
