b2b8fcb04acd2fb1ec6ed0b416ef5e0baa56432c
Dmitri G. Brengauz <dmitri@momus.net>
2016-01-17 14:40:58 -0700

    Users index

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -40,5 +40,13 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
+  test "there should be a reasonable number of tests" do
+    true
+  end
   
 end
